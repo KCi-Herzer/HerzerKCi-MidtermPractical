8810b7f730c471d001cb40652b0c919285dd7923
KCi-Herzer <neonhippie.kci@gmail.com>
2022-06-15 21:32:32 -0700

    Initial Dialoge

--- HerzerKCi-MidtermPractical/HerzerKCi-MidtermPractical.cpp
+++ HerzerKCi-MidtermPractical/HerzerKCi-MidtermPractical.cpp
@@ -3,6 +3,23 @@
 
 int main()
 {
-    std::cout << "Hello Professor!\nWelcome to my Midterm Practical\nPick an option:\n";
+    QuestionFactory factory;
+    std::cout << "Hello GitHub!\nWelcome to my Midterm Practical";
+    int choice = 0;
+    while (true)
+    {
+        std::cout << "\n\n1) Easy\n\nPick a Difficulty: ";
+        std::cin >> choice;
+        if (choice != 1)
+        {
+            std::cout << "Not an option..." << '\n';
+        }
+        else
+        {
+            //std::cout << "Good choice" << '\n';
+            break;
+        }
+    }
+    
     
 }
